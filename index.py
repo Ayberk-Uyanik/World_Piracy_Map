328750b4c83c86c528d50a18eb8087e105d3c1fc
Ayberk Uyanik <67552104+Ayberk-Uyanik@users.noreply.github.com>
2024-09-13 22:49:32 +0300

    Update index.py

--- index.py
+++ index.py
@@ -15,6 +15,26 @@ from components import navbar, footer
 nav = navbar.Navbar()
 footer_content = footer.Footer()
 
+# App Initiation #
+app = Dash(__name__, 
+           external_scripts=["https://cdn.plot.ly/plotly-2.18.2.min.js", 
+                            "https://raw.githack.com/eKoopmans/html2pdf/master/dist/html2pdf.bundle.js"
+            ], 
+            external_stylesheets=[
+               bootstrap, 
+               bootstrap_icons,
+               local_css
+            ],
+           use_pages=True,  
+           meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+           suppress_callback_exceptions = True,
+           url_base_pathname="/pages/Global_Outlook2/"
+)
+
+server = app.server
+
+app.title = "World Piracy Map"
+
 # Define the index page layout
 app.layout = html.Div([
     dcc.Location(
