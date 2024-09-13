04e747ea79ac36f5d1fa97133f3f0f73e300478b
Ayberk Uyanik <67552104+Ayberk-Uyanik@users.noreply.github.com>
2024-09-13 11:54:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ app = Dash(__name__,
             ],
            use_pages=True,  
            meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
-           suppress_callback_exceptions = True
+           suppress_callback_exceptions = True,
+           server=server
 )
 
 server = app.server
