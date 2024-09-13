4e2f85ed4b05506832af77ed7cf7b77bfe036366
Ayberk Uyanik <67552104+Ayberk-Uyanik@users.noreply.github.com>
2024-09-13 14:05:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,7 @@ app = Dash(__name__,
            use_pages=True,  
            meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
            suppress_callback_exceptions = True,
-           url_base_pathname="/pages/Global_Outlook2/",
-           use_pages=True
+           url_base_pathname="/pages/Global_Outlook2/"
 )
 
 server = app.server
