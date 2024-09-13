2d3f38bc4e827e1825d863c0f2e741099de6254b
Ayberk Uyanik <67552104+Ayberk-Uyanik@users.noreply.github.com>
2024-09-13 11:33:07 +0300

    Update index.py

--- index.py
+++ index.py
@@ -51,7 +51,5 @@ def display_page(pathname):
 # Run the app on localhost:8050
 if __name__ == '__main__':
     app.run_server(
-        debug=False,
-        host="0.0.0.0", 
-        port="8080"
+        debug=False
     )
